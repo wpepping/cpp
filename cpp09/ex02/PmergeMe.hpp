cced2989f4d0021378bd1ad4c16624a910eb88e8
Wouter Pepping <wpepping@live.nl>
2025-03-12 12:33:31 +0100

    .

---
+++ cpp09/ex02/PmergeMe.hpp
@@ -0,0 +1,15 @@
+#ifndef __PMERGME_H__
+#define __PMERGME_H__
+
+#include <vector>
+
+class PmergeMe {
+public:
+	static std::vector<int> sort(std::vector<int> container);
+
+private:
+	PmergeMe();
+	~PmergeMe();
+};
+
+#endif
---
+++ cpp09/ex02/main.cpp
@@ -0,0 +1,66 @@
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <vector>
+#include "PmergeMe.hpp"
+
+class ConversionFailedException : std::exception {
+	const char* what() const throw() {
+		return ("Conversion failed");
+	}
+};
+
+int err_handl(std::string message, int error_code) {
+	std::cout << "Error: " << message << std::endl;
+	return error_code;
+}
+
+void printContainer(const std::vector<int> &vec) {
+	for (size_t i = 0; i < vec.size(); ++i) {
+		std::cout << vec[i];
+		if (i < vec.size() - 1)
+			std::cout << ' ';
+	}
+}
+
+bool isDigits(std::string str) {
+	size_t	i;
+
+	for (i = 0; i < str.length(); i++)
+	{
+		if (!isdigit(str[i]))
+			return false;
+	}
+	return (i > 0);
+}
+
+std::vector<int> convertInput(unsigned int argc, char *argv[]) {
+	long				l;
+	char				*str_end;
+	std::vector<int>	result;
+
+	result.reserve(argc - 1);
+	for (size_t i = 1; i < argc; i++) {
+		if (!isDigits(argv[i]))
+			throw ConversionFailedException();
+		l = strtol(argv[i], &str_end, 10);
+		if (argv[i] == str_end || l < std::numeric_limits<int>::min()
+			|| l > std::numeric_limits<int>::max())
+			throw ConversionFailedException();
+		result.push_back(static_cast<int>(l));
+	}
+	return result;
+}
+
+int main(int argc, char *argv[]) {
+	std::vector<int> input;
+
+	try {
+		input = convertInput(argc, argv);
+	} catch (ConversionFailedException &e) {
+		return err_handl("Incorrect arguments", 1);
+	}
+	std::cout << "Input: ";
+	printContainer(input);
+	std::cout << std::endl;
+}
